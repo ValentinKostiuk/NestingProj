4861afbfea539f16958a43bf7116b35f265b1a00
ValentinKostyuk <valentin.kostiuk@gmail.com>
2014-09-06 23:37:29 +0300

    Re-style testRunner.html

--- TestsProj/geometryCoreTest/primitivesTests/Line.classTests.js
+++ TestsProj/geometryCoreTest/primitivesTests/Line.classTests.js
@@ -3,34 +3,35 @@
  */
 define (function (require, exports, module) {
 	'use strict';
-	var Line = require('Line').Line;
+	var Line = require ('Line').Line;
 
-	describe('Line', function () {
+	describe ('Line Class Tests', function () {
 
-		it('should move', function () {
+		it ('should move', function () {
 
 			// arrange
-			var line = new Line({ x1: 0, x2: 2, y1: 0, y2: 2 });
+			var line = new Line ({ x1: 0, x2: 2, y1: 0, y2: 2 });
 
 			// act
-			line.move(2, 0);
+			line.move (2, 0);
 
 			// assert
-			expect(line.x1).toBe(2);
-			expect(line.x2).toBe(4);
+			expect (line.x1).toBe (2);
+			expect (line.x2).toBe (4);
 		});
 
-		it('should rotateAround', function () {
+		it ('should rotateAround', function () {
 
 			// arrange
-			var line = new Line({ x1: 0, x2: 2, y1: 0, y2: 2 });
+			var line = new Line ({ x1: 0, x2: 2, y1: 0, y2: 2 });
 
 			// act
-			line.rotateAround(0, 0, 180);
+			line.rotateAround (0, 0, 180);
 
 			// assert
-			expect(line.x1).toBeCloseTo(0, 15);
-			expect(line.x2).toBeCloseTo(-2, 15);
+			expect (line.x1).toBeCloseTo (0, 15);
+			expect (line.x2).toBeCloseTo (-2, 15);
 		});
 	});
+
 });
