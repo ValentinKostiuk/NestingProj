79cdadc4dde79752ca4c2efaef81c0fb3c8c8772
ValentinKostyuk <valentin.kostiuk@gmail.com>
2014-09-07 01:57:36 +0300

    adding more tests for Line.class.js

--- TestsProj/geometryCoreTest/primitivesTests/Line.classTests.js
+++ TestsProj/geometryCoreTest/primitivesTests/Line.classTests.js
@@ -7,30 +7,51 @@ define (function (require, exports, module) {
 
 	describe ('Line Class Tests', function () {
 
-		it ('should move', function () {
-
-			// arrange3
-			var line = new Line ({ x1: 0, x2: 2, y1: 0, y2: 2 });
-
-			// act
-			line.move (2, 0);
-
-			// assert
-			expect (line.x1).toBe (2);
-			expect (line.x2).toBe (4);
+		it ('should be function', function(){
+			expect (typeof Line).toBe('function');
 		});
 
-		it ('should rotateAround', function () {
-
+		it ('result of call should be object type of line', function () {
 			// arrange
 			var line = new Line ({ x1: 0, x2: 2, y1: 0, y2: 2 });
-
-			// act
-			line.rotateAround (0, 0, 180);
-
 			// assert
-			expect (line.x1).toBeCloseTo (0, 15);
-			expect (line.x2).toBeCloseTo (-2, 15);
+			expect (line instanceof Line).toBeTruthy();
+		});
+
+		describe ('Line creation', function () {
+
+			it ('should set properties correctly', function () {
+				// arrange
+				var line = new Line ({ x1: 0, x2: 2, y1: 0, y2: 2 });
+				// assert
+				expect (line.x1).toBe (0);
+				expect (line.x2).toBe (2);
+				expect (line.y1).toBe (0);
+				expect (line.x2).toBe (2);
+			});
+
+			it ('should correctly calculate maxX & maxY coordinates of line', function () {
+				// arrange
+				var line = new Line ({ x1: 0, x2: 2, y1: 0, y2: 8 });
+				// assert
+				expect (line.maxX).toBe (2);
+				expect (line.maxY).toBe (8);
+			});
+
+			it('should correctly calculate minX & minY coordinates of line', function(){
+				// arrange
+				var line = new Line ({ x1: 1, x2: 2, y1: 0, y2: 8 });
+				// assert
+				expect (line.minX).toBe (1);
+				expect (line.minY).toBe (0);
+			});
+
+			it('should correctly calculate length of line', function(){
+				// arrange
+				var line = new Line ({ x1: 1, x2: 2, y1: 0, y2: 8 });
+				// assert
+				expect (line.length).toBeCloseTo(8.062258, 6);
+			});
 		});
 	});
 
